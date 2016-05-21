8bd94efc1a23d9c8d7e7acd53825e79260bd66f4
talhaobject90 <talhaqqq@gmail.com>
2016-05-21 15:56:43 +0530

    before pushing to origin

--- app/routes/dashboard/projects/home.js
+++ app/routes/dashboard/projects/home.js
@@ -12,5 +12,12 @@ setupController:function(controller){
   if(Ember.isEqual('gm', this.get('session.data.authenticated.role'))){
     controller.set('isGm',true );
   }
+
+  if(Ember.isEqual('is', this.get('session.data.authenticated.role'))){
+    controller.set('isIs',true );
+  }
+  if(Ember.isEqual('ps', this.get('session.data.authenticated.role'))){
+    controller.set('isPs',true );
+  }
 }
 });
--- app/routes/dashboard/projects/home/pen-qtns.js
+++ app/routes/dashboard/projects/home/pen-qtns.js
@@ -9,7 +9,9 @@ export default Ember.Route.extend({
      projects: this.store.findAll('project' ,{reload: true}).then(function(projects){
           return {
             pendingQuotationsOM:  projects.filterBy('fstatusom', 'forwarded'),
-            pendingQuotationsGM:  projects.filterBy('fstatusgm', 'forwarded')
+            pendingQuotationsGM:  projects.filterBy('fstatusgm', 'forwarded'),
+            pendingQuotationsIS:  projects.filterBy('fstatusis', 'forwarded'),
+            pendingQuotationsPS:  projects.filterBy('fstatusps', 'forwarded')
         };
      }),
    });
@@ -21,12 +23,21 @@ export default Ember.Route.extend({
       controller.set('projects',model.projects);
       controller.set('pendingQuotationsOM',model.projects.pendingQuotationsOM);
       controller.set('pendingQuotationsGM',model.projects.pendingQuotationsGM);
-      
+      controller.set('pendingQuotationsIS',model.projects.pendingQuotationsIS);
+      controller.set('pendingQuotationsPS',model.projects.pendingQuotationsPS);
+
       if(Ember.isEqual('om', this.get('session.data.authenticated.role'))){
         controller.set('isOm',true );
       }
       if(Ember.isEqual('gm', this.get('session.data.authenticated.role'))){
         controller.set('isGm',true );
+      }
+
+      if(Ember.isEqual('is', this.get('session.data.authenticated.role'))){
+        controller.set('isIs',true );
+      }
+      if(Ember.isEqual('ps', this.get('session.data.authenticated.role'))){
+        controller.set('isPs',true );
       }
 
   }
