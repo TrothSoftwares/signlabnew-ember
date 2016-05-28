69c796ba5679b0de1016950876bfbddb2e67d61e
talhaobject90 <talhaqqq@gmail.com>
2016-05-28 15:24:07 +0530

    after adding css for design and site

--- app/controllers/dashboard/customers/customer.js
+++ app/controllers/dashboard/customers/customer.js
@@ -3,16 +3,16 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   actions:{
-    editAgent:function(){
-      Ember.$('.agentview').transition('hide');
-      Ember.$('.agentedit').transition('show');
+    editCustomer:function(){
+      Ember.$('.customerview').transition('hide');
+      Ember.$('.customeredit').transition('show');
     },
-    saveAgent:function(){
+    saveCustomer:function(){
       var controller = this;
 
-        this.get('agent').save().then(function(){
-          Ember.$('.agentview').transition('show');
-          Ember.$('.agentedit').transition('hide');
+        this.get('customer').save().then(function(){
+          Ember.$('.customerview').transition('show');
+          Ember.$('.customeredit').transition('hide');
         }).catch(function(){
           controller.notifications.addNotification({
             message: 'Sorry, cant save at the moment !' ,
